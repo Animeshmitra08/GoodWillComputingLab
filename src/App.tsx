622407603732507af6326936362ced4e6937a3a6
Animeshmitra08 <animeshmitra003@gmail.com>
2025-11-24 12:42:08 +0530

    dshv bhbn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
 import LandingPage from './Components/LandingPage';
 import TeamMembers from './Components/TeamMembers';
 import Footer from './Components/Footer';
